0199b7a709782173ed8764efded0581705b46e1f
hravat <hussain.ravat786@gmail.com>
2024-03-18 16:20:17 +1300

    Complete airflow with port change

---
+++ docker_scripts/pp_loc_airflow/pp_loc_airflow_new.py
@@ -0,0 +1,71 @@
+from datetime import datetime,timedelta
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.sensors.bash import BashSensor
+from airflow.utils.trigger_rule import TriggerRule
+
+
+default_args = {
+
+    'owner': 'airflow',
+
+    'depends_on_past': False,
+
+    'start_date': datetime(2024, 3, 16),
+
+    'email_on_failure': False,
+
+    'email_on_retry': False,
+
+    'retries': 0,
+    
+    'execution_timeout': timedelta(minutes=7)
+
+}
+
+
+
+dag = DAG(
+
+    'pp_loc_airflow_new',
+
+    default_args=default_args,
+
+    description='A DAG to execute a bash script',
+
+    schedule_interval='@daily',  # Define your desired schedule here
+
+)
+
+
+
+# Define your bash command here
+
+bash_command_up = 'timeout 5m docker-compose -f /media/sf_data_engineering/group_project/data_engineering_petrol_pump/docker_scripts/docker-compose-petrol-pump.yaml up'   
+
+
+
+# Define the BashOperator to execute the bash script
+
+execute_bash_script = BashOperator(
+
+    task_id='docker_compose_up',
+    
+
+    bash_command=bash_command_up,
+
+    dag=dag,
+
+)
+
+bash_command_down = 'timeout 5m docker-compose -f /media/sf_data_engineering/group_project/data_engineering_petrol_pump/docker_scripts/docker-compose-petrol-pump.yaml down'   
+
+
+execute_bash_exit = BashOperator(task_id="docker_compose_down", bash_command=bash_command_down,dag=dag ,     trigger_rule=TriggerRule.ONE_FAILED,)
+
+
+execute_bash_script >> execute_bash_exit
+
+
+
+
--- docker_scripts/ppl_dockerfile/etl_pump_locations.py
+++ docker_scripts/ppl_dockerfile/etl_pump_locations.py
@@ -11,7 +11,7 @@ print(amenities)
 
 
 # establish connections
-conn_string = 'postgresql://postgres:postgres@172.17.0.1:5433/stg_db'
+conn_string = 'postgresql://postgres:postgres@172.17.0.1:5434/stg_db'
 
 db = create_engine(conn_string)
 conn = db.connect()
@@ -20,21 +20,22 @@ conn1 = psycopg2.connect(
   user='postgres',
   password='postgres',
   host='172.17.0.1',
- port= '5433'
+ port= '5434'
 )
 
 conn1.autocommit = True
 cursor = conn1.cursor()
-
-cursor.execute('drop table if exists public.airlines_final')
-
-sql = '''CREATE TABLE public.airlines_final(id int ,
-day int ,airline char(20),destination char(20));'''
-
-cursor.execute(sql)
-
+#
+#cursor.execute('drop table if exists public.airlines_final')
+#
+#sql = '''CREATE TABLE public.airlines_final(id int ,
+#day int ,airline char(20),destination char(20));'''
+#
+#cursor.execute(sql)
+#
 drop_column_list = ['geometry','nodes']
 amenities = amenities.drop(columns=drop_column_list)
+#amenities['Timestamp'] = pd.Timestamp.now()
 
 #engine = create_engine('postgresql://username:password@hostname:port/database_name')
 amenities.to_sql('pump_locations', con=db, if_exists='replace', index=True)
